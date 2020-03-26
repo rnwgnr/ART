85aa50052560b7cd794cfd0eea01e30c89c4a2be
Alberto Griggio <agriggio@users.noreply.github.com>
2020-03-26 13:17:25 +0100

    texture boost: smoother action of "edge stopping" parameter

--- rtengine/iptextureboost.cc
+++ rtengine/iptextureboost.cc
@@ -26,6 +26,7 @@
 #include "labmasks.h"
 #include "array2D.h"
 #include "guidedfilter.h"
+#include "rescale.h"
 
 namespace rtengine {
 
@@ -33,15 +34,28 @@ namespace {
 
 void texture_boost(array2D<float> &Y, const rtengine::procparams::TextureBoostParams::Region &pp, double scale, bool multithread)
 {
-    int radius = std::max(int(pp.edgeStopping * 3.5f / scale + 0.5f), 1);
+    float fradius = pp.edgeStopping * 3.5f / scale;
+    int radius = std::max(int(fradius + 0.5f), 1);
+    float delta = radius / fradius;
+
     float epsilon = 0.001f;
     float s = pp.strength >= 0 ? pow_F(pp.strength / 2.f, 0.3f) * 2.f : pp.strength;
     float strength = s >= 0 ? 1.f + s : 1.f / (1.f - s);
     float strength2 = s >= 0 ? 1.f + s / 4.f: 1.f / (1.f - s / 4.f);
 
-    const int W = Y.width();
-    const int H = Y.height();
+    int W = Y.width();
+    int H = Y.height();
 
+    array2D<float> tmpY;
+    array2D<float> *src = &Y;
+    if (fradius > 1.f && delta > 1.01f) {
+        W = int(W * delta + 0.5f);
+        H = int(H * delta + 0.5f);
+        tmpY(W, H);
+        rescaleBilinear(Y, tmpY, multithread);
+        src = &tmpY;
+    }
+    
     array2D<float> mid(W, H);
     array2D<float> base(W, H);
 
@@ -59,18 +73,18 @@ void texture_boost(array2D<float> &Y, const rtengine::procparams::TextureBoostPa
         int x = 0;
 #ifdef __SSE2__
         for (; x < W-3; x += 4) {
-            vfloat v = LVFU(Y[y][x]);
-            STVFU(Y[y][x], v / v65535);
+            vfloat v = LVFU((*src)[y][x]);
+            STVFU((*src)[y][x], v / v65535);
         }
 #endif
         for (; x < W; ++x) {
-            float v = Y[y][x] / 65535.f;
-            Y[y][x] = v;
+            float v = (*src)[y][x] / 65535.f;
+            (*src)[y][x] = v;
         }
     }
 
     for (int i = 0; i < pp.iterations; ++i) {
-        guidedFilter(Y, Y, mid, radius, epsilon, multithread);
+        guidedFilter((*src), (*src), mid, radius, epsilon, multithread);
         guidedFilter(mid, mid, base, radius * 4, epsilon / 10.f, multithread);
         
 #ifdef _OPENMP
@@ -80,20 +94,20 @@ void texture_boost(array2D<float> &Y, const rtengine::procparams::TextureBoostPa
             int x = 0;
 #ifdef __SSE2__
             for (; x < W-3; x += 4) {
-                vfloat vy = LVFU(Y[y][x]);
+                vfloat vy = LVFU((*src)[y][x]);
                 vfloat vm = LVFU(mid[y][x]);
                 vfloat vb = LVFU(base[y][x]);
                 vfloat d = (vy - vm) * vstrength;
                 vfloat d2 = (vm - vb) * vstrength2;
-                STVFU(Y[y][x], vminf(vmaxf(vb + d + d2, ZEROV), vone));
+                STVFU((*src)[y][x], vminf(vmaxf(vb + d + d2, ZEROV), vone));
             }
 #endif
             for (; x < W; ++x) {
-                float d = Y[y][x] - mid[y][x];
+                float d = (*src)[y][x] - mid[y][x];
                 d *= strength;
                 float d2 = mid[y][x] - base[y][x];
                 d2 *= strength2;
-                Y[y][x] = LIM01(base[y][x] + d + d2);
+                (*src)[y][x] = LIM01(base[y][x] + d + d2);
             }
         }
     }
@@ -105,13 +119,17 @@ void texture_boost(array2D<float> &Y, const rtengine::procparams::TextureBoostPa
         int x = 0;
 #ifdef __SSE2__
         for (; x < W-3; x += 4) {
-            vfloat v = LVFU(Y[y][x]);
-            STVFU(Y[y][x], v * v65535);
+            vfloat v = LVFU((*src)[y][x]);
+            STVFU((*src)[y][x], v * v65535);
         }
 #endif
         for (; x < W; ++x) {
-            Y[y][x] = Y[y][x] * 65535.f;
+            (*src)[y][x] = (*src)[y][x] * 65535.f;
         }
+    }
+
+    if (src != &Y) {
+        rescaleBilinear(*src, Y, multithread);
     }
 }
 
