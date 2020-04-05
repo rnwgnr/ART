rnwgnr/ART

commit 7310eb64978bb1138edbdf02fa58fb64a9326e17

metadata: better handling of makernote tags
Tentative fix for #37

rtengine/metadata.cc
@@ -45,6 +45,17 @@ std::unique_ptr<Exiv2Metadata::ImageCache> Exiv2Metadata::cache_(nullptr);
 
 namespace {
 
+class Error: public Exiv2::AnyError {
+public:
+    Error(const std::string &msg): msg_(msg) {}
+    const char *what() const throw() { return msg_.c_str(); }
+    int code() const throw() { return 0; }
+
+private:
+    std::string msg_;
+};
+
+
 constexpr size_t IMAGE_CACHE_SIZE = 200;
 
 #ifdef WIN32
@@ -430,11 +441,14 @@ void Exiv2Metadata::saveToImage(const Glib::ustring &path) const
 {
     auto dst = open_exiv2(path);
     if (image_.get()) {
-        dst->setMetadata(*image_);
+        dst->setIptcData(image_->iptcData());
+        dst->setXmpData(image_->xmpData());
         if (merge_xmp_) {
             do_merge_xmp(dst.get());
         }
-        remove_unwanted(dst.get());
+        auto srcexif = image_->exifData();
+        remove_unwanted(srcexif);
+        dst->setExifData(srcexif);
     } else {
         dst->setExifData(exif_data_);
         dst->setIptcData(iptc_data_);
@@ -448,42 +462,29 @@ void Exiv2Metadata::saveToImage(const Glib::ustring &path) const
 }
 
 
-void Exiv2Metadata::remove_unwanted(Exiv2::Image *dst) const
-{
-    static const std::vector<std::string> keys = {
-        "Exif.Image.Orientation",
-        "Exif.Image2.JPEGInterchangeFormat",
-        "Exif.Image2.JPEGInterchangeFormatLength",
-        "Exif.Photo.MakerNote"
-    };
-    for (auto &k : keys) {
-        auto it = dst->exifData().findKey(Exiv2::ExifKey(k));
-        if (it != dst->exifData().end()) {
-            dst->exifData().erase(it);
-        }
-    }
-    static const std::vector<std::string> patterns = {
+void Exiv2Metadata::remove_unwanted(Exiv2::ExifData &dst) const
+{                
+    Exiv2::ExifThumb thumb(dst);
+    thumb.erase();
+
+    static const std::vector<std::string> badpatterns = {
         "Exif.Image.",
-        "Exif.Photo.",
-        "Exif.GPSInfo."
+        "Exif.SubImage"
     };
-    for (auto it = dst->exifData().begin(); it != dst->exifData().end(); ) {
+    
+    for (auto it = dst.begin(); it != dst.end(); ) {
         bool found = false;
-        for (auto &pp : patterns) {
-            if (it->key().find(pp) == 0) {
+        for (auto &p : badpatterns) {
+            if (it->key().find(p) == 0) {
+                it = dst.erase(it);
                 found = true;
                 break;
             }
         }
         if (!found) {
-            it = dst->exifData().erase(it);
-        } else {
             ++it;
         }
-    }
-                
-    Exiv2::ExifThumb thumb(dst->exifData());
-    thumb.erase();
+    }    
 }
 
 
@@ -544,15 +545,6 @@ void Exiv2Metadata::saveToXmp(const Glib::ustring &path) const
         }
     }
 
-    class Error: public Exiv2::AnyError {
-    public:
-        Error(const std::string &msg): msg_(msg) {}
-        const char *what() const throw() { return msg_.c_str(); }
-        int code() const throw() { return 0; }
-
-    private:
-        std::string msg_;
-    };
     if (err) {
         throw Error("error saving XMP sidecar " + path);
     }

rtengine/metadata.h
@@ -64,7 +64,7 @@ private:
     void do_merge_xmp(Exiv2::Image *dst) const;
     void import_exif_pairs(Exiv2::ExifData &out) const;
     void import_iptc_pairs(Exiv2::IptcData &out) const;
-    void remove_unwanted(Exiv2::Image *dst) const;
+    void remove_unwanted(Exiv2::ExifData &dst) const;
     
     Glib::ustring src_;
     bool merge_xmp_;

commit 75a49b8cb9db957dfe13e6911dcf6c83bb92045d

metadata: preserve standard exif tags (accidentally broken by 7310eb64978bb1138edbdf02fa58fb64a9326e17)

rtengine/metadata.cc
@@ -23,12 +23,12 @@
 #include <iostream>
 #include <unistd.h>
 #include <giomm.h>
+#include <set>
 
 #ifdef WIN32
 #  include <windows.h>
 #  include <io.h>
 #  include <fcntl.h>
-#  include <set>
 #endif
 
 #include "metadata.h"
@@ -467,22 +467,87 @@ void Exiv2Metadata::remove_unwanted(Exiv2::ExifData &dst) const
     Exiv2::ExifThumb thumb(dst);
     thumb.erase();
 
+    static const std::set<std::string> badtags = {
+        "Exif.Image.Orientation",
+        "Exif.Image2.JPEGInterchangeFormat",
+        "Exif.Image2.JPEGInterchangeFormatLength",
+        "Exif.Image.NewSubfileType",
+        "Exif.Image.SubfileType",
+        "Exif.Image.ImageWidth",
+        "Exif.Image.ImageLength",
+        "Exif.Image.BitsPerSample",
+        "Exif.Image.Compression",
+        "Exif.Image.PhotometricInterpretation",
+        "Exif.Image.Thresholding",
+        "Exif.Image.CellWidth",
+        "Exif.Image.CellLength",
+        "Exif.Image.FillOrder",
+        "Exif.Image.StripOffsets",
+        "Exif.Image.Orientation",
+        "Exif.Image.SamplesPerPixel",
+        "Exif.Image.RowsPerStrip",
+        "Exif.Image.StripByteCounts",
+        "Exif.Image.XResolution",
+        "Exif.Image.YResolution",
+        "Exif.Image.PlanarConfiguration",
+        "Exif.Image.GrayResponseUnit",
+        "Exif.Image.GrayResponseCurve",
+        "Exif.Image.T4Options",
+        "Exif.Image.T6Options",
+        "Exif.Image.ResolutionUnit",
+        "Exif.Image.PageNumber",
+        "Exif.Image.Predictor",
+        "Exif.Image.TileWidth",
+        "Exif.Image.TileLength",
+        "Exif.Image.TileOffsets",
+        "Exif.Image.TileByteCounts",
+        "Exif.Image.SubIFDs",
+        "Exif.Image.ExtraSamples",
+        "Exif.Image.SampleFormat",
+        "Exif.Image.SMinSampleValue",
+        "Exif.Image.SMaxSampleValue",
+        "Exif.Image.Indexed",
+        "Exif.Image.JPEGTables",
+        "Exif.Image.OPIProxy",
+        "Exif.Image.JPEGProc",
+        "Exif.Image.JPEGInterchangeFormat",
+        "Exif.Image.JPEGInterchangeFormatLength",
+        "Exif.Image.JPEGRestartInterval",
+        "Exif.Image.JPEGLosslessPredictors",
+        "Exif.Image.JPEGPointTransforms",
+        "Exif.Image.JPEGQTables",
+        "Exif.Image.JPEGDCTables",
+        "Exif.Image.JPEGACTables",
+        "Exif.Image.TIFFEPStandardID",
+        "Exif.Image.DNGVersion",
+        "Exif.Image.DNGBackwardVersion",
+        "Exif.Image.DNGPrivateData",
+        "Exif.Image.OriginalRawFileData",
+        "Exif.Image.SubTileBlockSize",
+        "Exif.Image.RowInterleaveFactor",
+        "Exif.Photo.ComponentsConfiguration",
+        "Exif.Photo.CompressedBitsPerPixel"
+    };
+
     static const std::vector<std::string> badpatterns = {
-        "Exif.Image.",
         "Exif.SubImage"
     };
     
     for (auto it = dst.begin(); it != dst.end(); ) {
-        bool found = false;
-        for (auto &p : badpatterns) {
-            if (it->key().find(p) == 0) {
-                it = dst.erase(it);
-                found = true;
-                break;
+        if (badtags.find(it->key()) != badtags.end()) {
+            it = dst.erase(it);
+        } else {
+            bool found = false;
+            for (auto &p : badpatterns) {
+                if (it->key().find(p) == 0) {
+                    it = dst.erase(it);
+                    found = true;
+                    break;
+                }
             }
-        }
-        if (!found) {
-            ++it;
+            if (!found) {
+                ++it;
+            }
         }
     }    
 }