f8e13531ade9b8cdb93e61d7406dd85fbfa0fcac
Alberto Griggio <agriggio@users.noreply.github.com>
2020-04-20 15:05:36 +0200

    channel mixer: added SSE version

--- rtengine/ipchmixer.cc
+++ rtengine/ipchmixer.cc
@@ -54,10 +54,11 @@ void get_mixer_matrix(const ChannelMixerParams &chmix, const Glib::ustring &work
         A3{ float(m[2][0]), float(m[2][1]), float(m[2][2]) }
     };
 
-    constexpr float D65_x = 0.3127266146810121f;
-    constexpr float D65_y = 0.32902313032606195f;
+    constexpr float D65_x = 0.3127f;
+    constexpr float D65_y = 0.3290f;
+
     
-    const A3 white = { D65_x, D65_y, 1.f - D65_x - D65_y };
+    const A3 D65_bb_white = { D65_x, D65_y, 1.f - D65_x - D65_y };
 
     const auto rgb2xy =
         [&](const A3 &rgb) -> A3
@@ -65,7 +66,7 @@ void get_mixer_matrix(const ChannelMixerParams &chmix, const Glib::ustring &work
             A3 xyz = dotProduct(ws, rgb);
             float sum = xyz[0] + xyz[1] + xyz[2];
             if (sum == 0.f) {
-                return white;
+                return D65_bb_white;
             }
             float x = xyz[0] / sum;
             float y = xyz[1] / sum;
@@ -73,7 +74,7 @@ void get_mixer_matrix(const ChannelMixerParams &chmix, const Glib::ustring &work
         };
     
     const auto get_matrix =
-        [&](const A3 &r, const A3 &g, const A3 &b) -> M33
+        [&](const A3 &r, const A3 &g, const A3 &b, const A3 &white) -> M33
         {
             auto r_xy = rgb2xy(r);
             auto g_xy = rgb2xy(g);
@@ -104,7 +105,7 @@ void get_mixer_matrix(const ChannelMixerParams &chmix, const Glib::ustring &work
     A3 green = {0.f, 1.f, 0.f};
     A3 blue = {0.f, 0.f, 1.f};
 
-    M33 M = get_matrix(red, green, blue);
+    M33 M = get_matrix(red, green, blue, D65_bb_white);
 
     const auto tweak =
         [](const A3 &c, int hue, int sat, float hrange) -> A3
@@ -131,7 +132,8 @@ void get_mixer_matrix(const ChannelMixerParams &chmix, const Glib::ustring &work
 
     M33 N = get_matrix(tweak(red, chmix.hue_tweak[0], chmix.sat_tweak[0], 0.05f),
                        tweak(green, chmix.hue_tweak[1], chmix.sat_tweak[1], 0.15f),
-                       tweak(blue, chmix.hue_tweak[2], chmix.sat_tweak[2], 0.15f));
+                       tweak(blue, chmix.hue_tweak[2], chmix.sat_tweak[2], 0.15f),
+                       D65_bb_white);
 
     M33 Minv;
     if (!invertMatrix(M, Minv)) {
@@ -169,46 +171,75 @@ void ImProcFunctions::channelMixer(Imagefloat *img)
     if (params->chmixer.enabled) {
         img->setMode(Imagefloat::Mode::RGB, multiThread);
         
-        float chMixRR = float(params->chmixer.red[0])/10.f;
-        float chMixRG = float(params->chmixer.red[1])/10.f;
-        float chMixRB = float(params->chmixer.red[2])/10.f;
-        float chMixGR = float(params->chmixer.green[0])/10.f;
-        float chMixGG = float(params->chmixer.green[1])/10.f;
-        float chMixGB = float(params->chmixer.green[2])/10.f;
-        float chMixBR = float(params->chmixer.blue[0])/10.f;
-        float chMixBG = float(params->chmixer.blue[1])/10.f;
-        float chMixBB = float(params->chmixer.blue[2])/10.f;
+        float RR = float(params->chmixer.red[0])/10.f;
+        float RG = float(params->chmixer.red[1])/10.f;
+        float RB = float(params->chmixer.red[2])/10.f;
+        float GR = float(params->chmixer.green[0])/10.f;
+        float GG = float(params->chmixer.green[1])/10.f;
+        float GB = float(params->chmixer.green[2])/10.f;
+        float BR = float(params->chmixer.blue[0])/10.f;
+        float BG = float(params->chmixer.blue[1])/10.f;
+        float BB = float(params->chmixer.blue[2])/10.f;
+
 
         if (params->chmixer.mode == ChannelMixerParams::Mode::PRIMARIES_CHROMA){
             get_mixer_matrix(params->chmixer, params->icm.workingProfile,
-                             chMixRR, chMixRG, chMixRB,
-                             chMixGR, chMixGG, chMixGB,
-                             chMixBR, chMixBG, chMixBB);
+                             RR, RG, RB,
+                             GR, GG, GB,
+                             BR, BG, BB);
             if (options.rtSettings.verbose) {
                 printf("Channel mixer matrix:\n"
                        "   %.1f %.1f %.1f\n"
                        "   %.1f %.1f %.1f\n"
                        "   %.1f %.1f %.1f\n",
-                       chMixRR, chMixRG, chMixRB,
-                       chMixGR, chMixGG, chMixGB,
-                       chMixBR, chMixBG, chMixBB);
+                       RR, RG, RB,
+                       GR, GG, GB,
+                       BR, BG, BB);
                 fflush(stdout);
             }
         }
 
-        
+#ifdef __SSE2__
+        vfloat vRR = F2V(RR);
+        vfloat vRG = F2V(RG);
+        vfloat vRB = F2V(RB);
+        vfloat vGR = F2V(GR);
+        vfloat vGG = F2V(GG);
+        vfloat vGB = F2V(GB);
+        vfloat vBR = F2V(BR);
+        vfloat vBG = F2V(BG);
+        vfloat vBB = F2V(BB);
+        vfloat v100 = F2V(100.f);
+#endif // __SSE2__
+
 #ifdef _OPENMP
 #       pragma omp parallel for if (multiThread)
 #endif
         for (int y = 0; y < img->getHeight(); ++y) {
-            for (int x = 0; x < img->getWidth(); ++x) {
+            int x = 0;
+#ifdef __SSE2__
+            for (; x < img->getWidth()-3; x += 4) {
+                vfloat r = LVF(img->r(y, x));
+                vfloat g = LVF(img->g(y, x));
+                vfloat b = LVF(img->b(y, x));
+
+                vfloat rmix = (r * vRR + g * vRG + b * vRB) / v100;
+                vfloat gmix = (r * vGR + g * vGG + b * vGB) / v100;
+                vfloat bmix = (r * vBR + g * vBG + b * vBB) / v100;
+
+                STVF(img->r(y, x), rmix);
+                STVF(img->g(y, x), gmix);
+                STVF(img->b(y, x), bmix);
+            }
+#endif
+            for (; x < img->getWidth(); ++x) {
                 float r = img->r(y, x);
                 float g = img->g(y, x);
                 float b = img->b(y, x);
 
-                float rmix = (r * chMixRR + g * chMixRG + b * chMixRB) / 100.f;
-                float gmix = (r * chMixGR + g * chMixGG + b * chMixGB) / 100.f;
-                float bmix = (r * chMixBR + g * chMixBG + b * chMixBB) / 100.f;
+                float rmix = (r * RR + g * RG + b * RB) / 100.f;
+                float gmix = (r * GR + g * GG + b * GB) / 100.f;
+                float bmix = (r * BR + g * BG + b * BB) / 100.f;
 
                 img->r(y, x) = rmix;
                 img->g(y, x) = gmix;
