rnwgnr/ART

commit cbb1314d4a45711b1e4d3efb745dcb2cb3b5d192

added option to control where to save the output parameters (sidecar or metadata)

rtgui/main-cli.cc
@@ -860,7 +860,9 @@ int processLineParams ( int argc, char **argv )
         } else {
             if (copyParamsFile) {
                 Glib::ustring outputProcessingParams = outputFile + paramFileExtension;
-                currentParams.save(nullptr, outputProcessingParams);
+                if (!options.params_out_embed || currentParams.saveEmbedded(outputFile) != 0) {
+                    currentParams.save(nullptr, outputProcessingParams);
+                }
             }
         }
 

rtdata/languages/default
@@ -1294,6 +1294,9 @@ PREFERENCES_PROFILESAVECACHE;Save processing profile to the cache
 PREFERENCES_PROFILESAVEINPUT;Save processing profile next to the input file
 PREFERENCES_PROFILESAVELOCATION;Processing profile saving location
 PREFERENCES_PROFILE_NONE;None
+PREFERENCES_PROFILE_SAVE_OUTPUT;Processing profiles for output images
+PREFERENCES_PROFILE_SAVE_OUTPUT_SIDECAR;Save in a sidecar file (output.ext.arp for output.ext)
+PREFERENCES_PROFILE_SAVE_OUTPUT_EMBEDDED;Embed in the metadata
 PREFERENCES_PRTINTENT;Rendering intent
 PREFERENCES_PRTPROFILE;Color profile
 PREFERENCES_PSPATH;Adobe Photoshop installation directory

rtgui/batchqueue.cc
@@ -719,8 +719,10 @@ rtengine::ProcessingJob* BatchQueue::imageReady(rtengine::IImagefloat* img)
             if (batch_profile_) {
                 batch_profile_->applyTo(processing->params);
             }
-            if (processing->params.saveEmbedded(this, fname) != 0) {
-                auto sidecar = fname + ".out" + paramFileExtension;
+            auto sidecar = fname + ".out" + paramFileExtension;
+            if (!options.params_out_embed) {
+                processing->params.save(this, sidecar);
+            } else if (processing->params.saveEmbedded(this, fname) != 0) {
                 error(Glib::ustring::compose(M("PROCPARAMS_EMBEDDED_SAVE_WARNING"), fname, sidecar));
                 processing->params.save(this, sidecar);
             }

rtgui/editorpanel.cc
@@ -1820,8 +1820,10 @@ bool EditorPanel::idle_imageSaved (ProgressConnector<int> *pc, rtengine::IImagef
         if (sf.saveParams) {
             // We keep the extension to avoid overwriting the profile when we have
             // the same output filename with different extension
-            if (pparams.saveEmbedded(parent, fname) != 0) { 
-                auto sidecar = fname + ".out" + paramFileExtension;
+            auto sidecar = fname + ".out" + paramFileExtension;
+            if (!options.params_out_embed) {
+                pparams.save(parent, sidecar);
+            } else if (pparams.saveEmbedded(parent, fname) != 0) { 
                 parent->error(Glib::ustring::compose(M("PROCPARAMS_EMBEDDED_SAVE_WARNING"), fname, sidecar));
                 pparams.save(parent, sidecar);
             }

rtgui/options.cc
@@ -371,6 +371,7 @@ void Options::setDefaults()
     saveParamsFile = true;              // was false, but saving the procparams files next to the file make more sense when reorganizing file tree than in a cache
     saveParamsCache = false;            // there's no need to save the procparams files in a cache if saveParamsFile is true
     paramsLoadLocation = PLL_Input;     // was PLL_Cache
+    params_out_embed = false;
     procQueueEnabled = false;
     gimpDir = "";
     psDir = "";
@@ -918,6 +919,10 @@ void Options::readFromFile(Glib::ustring fname)
                     paramsLoadLocation = (PPLoadLocation)keyFile.get_integer("Profiles", "LoadParamsFromLocation");
                 }
 
+                if (keyFile.has_key("Profiles", "EmbedParamsInMetadata")) {
+                    params_out_embed = keyFile.get_boolean("Profiles", "EmbedParamsInMetadata");
+                }
+                
                 if (keyFile.has_key("Profiles", "CustomProfileBuilder")) {
                     CPBPath = keyFile.get_string("Profiles", "CustomProfileBuilder");  // for backward compatibility only
                 }
@@ -2048,6 +2053,7 @@ void Options::saveToFile(Glib::ustring fname)
         keyFile.set_boolean("Profiles", "SaveParamsWithFile", saveParamsFile);
         keyFile.set_boolean("Profiles", "SaveParamsToCache", saveParamsCache);
         keyFile.set_integer("Profiles", "LoadParamsFromLocation", paramsLoadLocation);
+        keyFile.set_boolean("Profiles", "EmbedParamsInMetadata", params_out_embed);
         keyFile.set_string("Profiles", "CustomProfileBuilderPath", CPBPath);
         keyFile.set_integer("Profiles", "CustomProfileBuilderKeys", CPBKeys);
 

rtgui/options.h
@@ -242,6 +242,8 @@ public:
     bool saveParamsFile;
     bool saveParamsCache;
     PPLoadLocation paramsLoadLocation;
+    bool params_out_embed;
+    
     bool procQueueEnabled;
     Glib::ustring gimpDir;
     Glib::ustring psDir;

rtgui/preferences.cc
@@ -192,6 +192,7 @@ Gtk::Widget* Preferences::getImageProcessingPanel ()
     saveParamsPreference->append (M ("PREFERENCES_PROFILESAVECACHE"));
     saveParamsPreference->append (M ("PREFERENCES_PROFILESAVEBOTH"));
     Gtk::Label *splab = Gtk::manage (new Gtk::Label (M ("PREFERENCES_PROFILESAVELOCATION") + ":"));
+    setExpandAlignProperties(splab, false, false, Gtk::ALIGN_START, Gtk::ALIGN_CENTER);
     vbdp->attach (*splab, 0, 1, 0, 1, Gtk::FILL, Gtk::SHRINK, 2, 2);
     vbdp->attach (*saveParamsPreference, 1, 2, 0, 1, Gtk::EXPAND | Gtk::FILL | Gtk::SHRINK, Gtk::SHRINK, 2, 2);
     Gtk::Label* lplab = Gtk::manage (new Gtk::Label (M ("PREFERENCES_PROFILELOADPR") + ":"));
@@ -199,7 +200,17 @@ Gtk::Widget* Preferences::getImageProcessingPanel ()
     loadParamsPreference->append (M ("PREFERENCES_PROFILEPRCACHE"));
     loadParamsPreference->append (M ("PREFERENCES_PROFILEPRFILE"));
     vbdp->attach (*lplab, 0, 1, 1, 2, Gtk::FILL, Gtk::SHRINK, 2, 2);
+    setExpandAlignProperties(lplab, false, false, Gtk::ALIGN_START, Gtk::ALIGN_CENTER);
     vbdp->attach (*loadParamsPreference, 1, 2, 1, 2, Gtk::EXPAND | Gtk::FILL | Gtk::SHRINK, Gtk::SHRINK, 2, 2);
+
+    lplab = Gtk::manage(new Gtk::Label(M ("PREFERENCES_PROFILE_SAVE_OUTPUT") + ":"));
+    saveOutParamsPreference = Gtk::manage (new Gtk::ComboBoxText());
+    saveOutParamsPreference->append(M("PREFERENCES_PROFILE_SAVE_OUTPUT_SIDECAR"));
+    saveOutParamsPreference->append(M("PREFERENCES_PROFILE_SAVE_OUTPUT_EMBEDDED"));
+    vbdp->attach(*lplab, 0, 1, 2, 3, Gtk::FILL, Gtk::SHRINK, 2, 2);
+    setExpandAlignProperties(lplab, false, false, Gtk::ALIGN_START, Gtk::ALIGN_CENTER);
+    vbdp->attach(*saveOutParamsPreference, 1, 2, 2, 3, Gtk::EXPAND | Gtk::FILL | Gtk::SHRINK, Gtk::SHRINK, 2, 2);
+    
     fdp->add (*vbdp);
     vbImageProcessing->pack_start (*fdp, Gtk::PACK_SHRINK, 4);
 
@@ -1519,6 +1530,7 @@ void Preferences::storePreferences ()
     moptions.saveParamsFile = save_where == 0 || save_where == 2;
     moptions.saveParamsCache = save_where == 1 || save_where == 2;
     moptions.paramsLoadLocation = (PPLoadLocation)loadParamsPreference->get_active_row_number ();
+    moptions.params_out_embed = saveOutParamsPreference->get_active_row_number() == 1;
     moptions.useBundledProfiles = useBundledProfiles->get_active ();
 
     moptions.rtSettings.darkFramesPath = darkFrameDir->get_filename();
@@ -1747,8 +1759,9 @@ void Preferences::fillPreferences ()
 
     saveParamsPreference->set_active (moptions.saveParamsFile ? (moptions.saveParamsCache ? 2 : 0) : 1);
 
-    loadParamsPreference->set_active (moptions.paramsLoadLocation);
-    useBundledProfiles->set_active (moptions.useBundledProfiles);
+    loadParamsPreference->set_active(moptions.paramsLoadLocation);
+    saveOutParamsPreference->set_active(moptions.params_out_embed ? 1 : 0);
+    useBundledProfiles->set_active(moptions.useBundledProfiles);
 
     if (!moptions.tabbedUI) {
         editorLayout->set_active (moptions.mainNBVertical ? 1 : 0);

rtgui/preferences.h
@@ -161,6 +161,7 @@ class Preferences : public Gtk::Dialog, public ProfileStoreListener
     Gtk::ComboBoxText* saveParamsPreference;
     Gtk::CheckButton* useBundledProfiles;
     Gtk::ComboBoxText* loadParamsPreference;
+    Gtk::ComboBoxText *saveOutParamsPreference;
     Gtk::ComboBoxText* editorLayout;
     RTWindow* parent;
 

commit 07ed9a6c9ffa5dc5de682945f63ababbee0bf51b

fixed compilation (accidentally broken by cbb1314d4a45711b1e4d3efb745dcb2cb3b5d192)

rtgui/main-cli.cc
@@ -860,8 +860,8 @@ int processLineParams ( int argc, char **argv )
         } else {
             if (copyParamsFile) {
                 Glib::ustring outputProcessingParams = outputFile + paramFileExtension;
-                if (!options.params_out_embed || currentParams.saveEmbedded(outputFile) != 0) {
-                    currentParams.save(nullptr, outputProcessingParams);
+                if (!options.params_out_embed || currentParams.saveEmbedded(pl, outputFile) != 0) {
+                    currentParams.save(pl, outputProcessingParams);
                 }
             }
         }