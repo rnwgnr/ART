18efebc433f9d324d9c5fc9c397ab39a1d6e03b0
Alberto Griggio <agriggio@users.noreply.github.com>
2020-09-26 23:35:53 +0200

    tone equalizer: revised regularization formula for values > 1

--- rtengine/iptoneequalizer.cc
+++ rtengine/iptoneequalizer.cc
@@ -133,7 +133,7 @@ void tone_eq(array2D<float> &R, array2D<float> &G, array2D<float> &B, const Tone
 
     if (pp.regularization > 0) {
         array2D<float> Y2(W, H);
-        constexpr float base_epsilon = 0.02f;
+        constexpr float base_epsilon = 0.004f;
         constexpr float base_posterization = 5.f;
         
 #ifdef _OPENMP
@@ -148,8 +148,12 @@ void tone_eq(array2D<float> &R, array2D<float> &G, array2D<float> &B, const Tone
             }
         }
         radius = 350.f / scale;
-        epsilon = base_epsilon / float(6 - std::min(pp.regularization, 5));
-        rtengine::guidedFilter(Y2, Y, Y, radius, epsilon, multithread);        
+        epsilon = base_epsilon;
+        rtengine::guidedFilter(Y2, Y, Y, radius, epsilon, multithread);
+
+        if (pp.regularization > 1) {
+            rtengine::guidedFilter(Y2, Y, Y, radius * (pp.regularization - 1), epsilon / 100, multithread);
+        }
     }
     
     const auto gauss =
