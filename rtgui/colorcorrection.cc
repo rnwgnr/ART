rnwgnr/ART

commit 3d00f84dfbe0d1b7580800dc5ddb1d5ba386b789

local editing: disable on-canvas editing when tools are switched off

rtgui/colorcorrection.cc
@@ -484,6 +484,10 @@ void ColorCorrection::enabledChanged ()
         box_rgb->set_sensitive(getEnabled());
         box_hsl->set_sensitive(getEnabled());
     }
+
+    if (!getEnabled()) {
+        labMasks->switchOffEditMode();
+    }
 }
 
 

rtgui/localcontrast.cc
@@ -273,6 +273,10 @@ void LocalContrast::enabledChanged ()
             listener->panelChanged(EvLocalContrastEnabled, M("GENERAL_DISABLED"));
         }
     }
+
+    if (!getEnabled()) {
+        labMasks->switchOffEditMode();
+    }
 }
 
 

rtgui/smoothing.cc
@@ -313,6 +313,10 @@ void Smoothing::enabledChanged ()
             listener->panelChanged(EvEnabled, M("GENERAL_DISABLED"));
         }
     }
+
+    if (!getEnabled()) {
+        labMasks->switchOffEditMode();
+    }
 }
 
 

rtgui/textureboost.cc
@@ -265,6 +265,10 @@ void TextureBoost::enabledChanged ()
             listener->panelChanged (EvEPDEnabled, M("GENERAL_DISABLED"));
         }
     }
+
+    if (!getEnabled()) {
+        labMasks->switchOffEditMode();
+    }
 }
 
 

rtgui/labmaskspanel.cc
@@ -1600,6 +1600,9 @@ bool LabMasksPanel::onKnotRoundnessUpdated()
 
 void LabMasksPanel::switchOffEditMode()
 {
+    static_cast<DeltaEArea *>(deltaEColor)->switchOffEditMode();
+    static_cast<DrawnMaskPanel *>(drawnMask)->switchOffEditMode();
+    
     areaMaskToggle->set_active(false);
     AreaMask::switchOffEditMode();
 }
@@ -2441,3 +2444,4 @@ void LabMasksPanel::onMaskExpanded(GdkEventButton *evt, MyExpander *exp)
         }
     }
 }
+

rtgui/labmaskspanel.h
@@ -137,7 +137,7 @@ public:
     void adjusterChanged(ThresholdAdjuster *a, int newBottom, int newTop) override {}
     void adjusterChanged(ThresholdAdjuster *a, int newBottomLeft, int newTopLeft, int newBottomRight, int newTopRight) override {}
     void adjusterChanged2(ThresholdAdjuster *a, int newBottomL, int newTopL, int newBottomR, int newTopR) override {}
-    
+
 private:
     void on_map() override;
     void onMaskFold(GdkEventButton *evt);

commit f302a2c89f4d9d716027aeed353cfe22ed113655

fixed crash introduced by 3d00f84dfbe0d1b7580800dc5ddb1d5ba386b789

rtgui/colorcorrection.cc
@@ -485,7 +485,7 @@ void ColorCorrection::enabledChanged ()
         box_hsl->set_sensitive(getEnabled());
     }
 
-    if (!getEnabled()) {
+    if (listener && !getEnabled()) {
         labMasks->switchOffEditMode();
     }
 }

rtgui/localcontrast.cc
@@ -274,7 +274,7 @@ void LocalContrast::enabledChanged ()
         }
     }
 
-    if (!getEnabled()) {
+    if (listener && !getEnabled()) {
         labMasks->switchOffEditMode();
     }
 }

rtgui/smoothing.cc
@@ -314,7 +314,7 @@ void Smoothing::enabledChanged ()
         }
     }
 
-    if (!getEnabled()) {
+    if (listener && !getEnabled()) {
         labMasks->switchOffEditMode();
     }
 }

rtgui/textureboost.cc
@@ -266,7 +266,7 @@ void TextureBoost::enabledChanged ()
         }
     }
 
-    if (!getEnabled()) {
+    if (listener && !getEnabled()) {
         labMasks->switchOffEditMode();
     }
 }