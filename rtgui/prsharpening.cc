fd5707564b6c7f95ea7783c73084d9986ce99faf
Alberto Griggio <agriggio@users.noreply.github.com>
2021-01-02 18:55:20 +0100

    fixed pipeline refresh for output sharpening

--- rtgui/prsharpening.cc
+++ rtgui/prsharpening.cc
@@ -26,8 +26,8 @@ using namespace rtengine::procparams;
 PrSharpening::PrSharpening () : FoldableToolPanel(this, "prsharpening", M("TP_PRSHARPENING_LABEL"), false, true, true)
 {
     auto m = ProcEventMapper::getInstance();
-    EvPrShrContrast = m->newEvent(RESIZE, "HISTORY_MSG_PRSHARPEN_CONTRAST");
-    EvToolReset.set_action(RESIZE);
+    EvPrShrContrast = m->newEvent(M_LUMINANCE, "HISTORY_MSG_PRSHARPEN_CONTRAST");
+    EvToolReset.set_action(M_LUMINANCE);
 
     std::vector<GradientMilestone> milestones;
     milestones.push_back( GradientMilestone(0.0, 0.0, 0.0, 0.0) );
