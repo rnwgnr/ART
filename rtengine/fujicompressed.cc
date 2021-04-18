73f883affb0fb755c742c2ae41e94c0fd791047e
Thanatomanic <6567747+Thanatomanic@users.noreply.github.com>
2021-04-18 13:40:44 +0200

    Apply patch to support 16-bit Fujifilm + support for GFX100S

--- rtengine/fujicompressed.cc
+++ rtengine/fujicompressed.cc
@@ -25,7 +25,7 @@ int bitDiff (int value1, int value2)
     int decBits = 0;
 
     if ( value2 < value1 )
-        while (decBits <= 12 && (value2 << ++decBits) < value1)
+        while (decBits <= 14 && (value2 << ++decBits) < value1)
             ;
 
     return decBits;
