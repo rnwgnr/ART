42673189141757ccf8001ad5c65e06860c1db326
Alberto Griggio <agriggio@users.noreply.github.com>
2022-03-26 14:51:29 +0100

    dcraw: allow linear_table for sizes up to 16 bits (ported from LibRaw)

--- rtengine/dcraw.cc
+++ rtengine/dcraw.cc
@@ -6380,11 +6380,14 @@ void CLASS parse_mos (int offset)
 void CLASS linear_table (unsigned len)
 {
   int i;
-  if (len > 0x1000) len = 0x1000;
-  read_shorts (curve, len);
-  for (i=len; i < 0x1000; i++)
-    curve[i] = curve[i-1];
-  maximum = curve[0xfff];
+  if (len > 0x10000)
+    len = 0x10000;
+  else if (len < 1)
+    return;
+  read_shorts(curve, len);
+  for (i = len; i < 0x10000; i++)
+    curve[i] = curve[i - 1];
+  maximum = curve[len < 0x1000 ? 0xfff : len - 1];
 }
 
 void CLASS parse_kodak_ifd (int base)
