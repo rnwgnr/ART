a7e4ef71f9b400ffd53532c91d2ecb4e17e5ce2a
Alberto Griggio <agriggio@users.noreply.github.com>
2022-04-13 14:54:30 +0200

    tweaked lens identification for Canon RF cameras

--- rtengine/imagedata.cc
+++ rtengine/imagedata.cc
@@ -266,9 +266,6 @@ FramesData::FramesData(const Glib::ustring &fname):
             auto p = pos;
             if (find_exif_tag("Exif.CanonFi.RFLensType") && find_exif_tag("Exif.Canon.LensModel")) {
                 lens = pos->print(&exif);
-                if (Glib::ustring(lens).lowercase().find("canon") == Glib::ustring::npos) {
-                    lens = std::string("Canon ") + lens;
-                }
             } else if (p->count() == 1 && lens == std::to_string(p->toLong()) &&
                 find_exif_tag("Exif.Photo.LensModel")) {
                 lens = p->print(&exif);
