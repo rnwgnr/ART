rnwgnr/ART

commit 7af982c513a6cda947a769c5b82f17c1d6db7da0

added option to put the filmstrip below the editor panel in single-editor mode

rtgui/batchqueuepanel.cc
@@ -235,7 +235,7 @@ void BatchQueuePanel::updateTab (int qsize, int forceOrientation)
     }
 
     Gtk::Grid* grid = Gtk::manage (new Gtk::Grid ());
-    if ((forceOrientation == 0 && options.mainNBVertical) || (forceOrientation == 2)) {
+    if ((forceOrientation == 0) || (forceOrientation == 2)) {
         Gtk::Label* l;
 
         if(!qsize ) {

rtgui/filecatalog.cc
@@ -920,7 +920,7 @@ void FileCatalog::_refreshProgressBar ()
         Gtk::Label *label = nullptr;
 
         int tot = previewsToLoad ? previewsToLoad : previewsLoaded;
-        grid->attach_next_to(*Gtk::manage(new RTImage("folder-closed.png")), options.mainNBVertical ? Gtk::POS_TOP : Gtk::POS_RIGHT, 1, 1);
+        grid->attach_next_to(*Gtk::manage(new RTImage("folder-closed.png")), Gtk::POS_TOP, 1, 1);
         int filteredCount = fileBrowser->getNumFiltered() < 0 ? tot : min(fileBrowser->getNumFiltered(), tot);
 
         label = Gtk::manage(new Gtk::Label(M("MAIN_FRAME_FILEBROWSER") +
@@ -928,25 +928,9 @@ void FileCatalog::_refreshProgressBar ()
                                                + Glib::ustring::format(tot) +
                                                (filteredCount != tot ? "]" : ")")));
 
-        // if (!previewsToLoad) {
-        //     grid->attach_next_to(*Gtk::manage(new RTImage("folder-closed.png")), options.mainNBVertical ? Gtk::POS_TOP : Gtk::POS_RIGHT, 1, 1);
-        //     int filteredCount = min(fileBrowser->getNumFiltered(), previewsLoaded);
+        label->set_angle(90);
 
-        //     label = Gtk::manage(new Gtk::Label(M("MAIN_FRAME_FILEBROWSER") +
-        //                                        (filteredCount != previewsLoaded ? " [" + Glib::ustring::format(filteredCount) + "/" : " (")
-        //                                        + Glib::ustring::format(previewsLoaded) +
-        //                                        (filteredCount != previewsLoaded ? "]" : ")")));
-        // } else {
-        //     grid->attach_next_to(*Gtk::manage(new RTImage("magnifier.png")), options.mainNBVertical ? Gtk::POS_TOP : Gtk::POS_RIGHT, 1, 1);
-        //     label = Gtk::manage(new Gtk::Label(M("MAIN_FRAME_FILEBROWSER") + " [" + Glib::ustring::format(std::fixed, std::setprecision(0), std::setw(3), (double)previewsLoaded / previewsToLoad * 100 ) + "%]" ));
-        //     filepanel->loadingThumbs("", (double)previewsLoaded / previewsToLoad);
-        // }
-
-        if (options.mainNBVertical) {
-            label->set_angle(90);
-        }
-
-        grid->attach_next_to(*label, options.mainNBVertical ? Gtk::POS_TOP : Gtk::POS_RIGHT, 1, 1);
+        grid->attach_next_to(*label, Gtk::POS_TOP, 1, 1);
         grid->set_tooltip_markup(M("MAIN_FRAME_FILEBROWSER_TOOLTIP"));
         grid->show_all();
 

rtgui/rtwindow.cc
@@ -519,17 +519,11 @@ RTWindow::RTWindow():
         setExpandAlignProperties (fpanelLabelGrid, false, false, Gtk::ALIGN_CENTER, Gtk::ALIGN_CENTER);
         Gtk::Label* fpl = Gtk::manage (new Gtk::Label ( Glib::ustring (" ") + M ("MAIN_FRAME_EDITOR") ));
 
-        if (options.mainNBVertical) {
-            mainNB->set_tab_pos (Gtk::POS_LEFT);
-            fpl->set_angle (90);
-            RTImage* folderIcon = Gtk::manage (new RTImage ("folder-closed.png"));
-            fpanelLabelGrid->attach_next_to (*folderIcon, Gtk::POS_TOP, 1, 1);
-            fpanelLabelGrid->attach_next_to (*fpl, Gtk::POS_TOP, 1, 1);
-        } else {
-            RTImage* folderIcon = Gtk::manage (new RTImage ("folder-closed.png"));
-            fpanelLabelGrid->attach_next_to (*folderIcon, Gtk::POS_RIGHT, 1, 1);
-            fpanelLabelGrid->attach_next_to (*fpl, Gtk::POS_RIGHT, 1, 1);
-        }
+        mainNB->set_tab_pos (Gtk::POS_LEFT);
+        fpl->set_angle (90);
+        RTImage* folderIcon = Gtk::manage (new RTImage ("folder-closed.png"));
+        fpanelLabelGrid->attach_next_to (*folderIcon, Gtk::POS_TOP, 1, 1);
+        fpanelLabelGrid->attach_next_to (*fpl, Gtk::POS_TOP, 1, 1);
 
         fpanelLabelGrid->set_tooltip_markup (M ("MAIN_FRAME_FILEBROWSER_TOOLTIP"));
         fpanelLabelGrid->show_all ();
@@ -542,10 +536,7 @@ RTWindow::RTWindow():
         // decorate tab, the label is unimportant since its updated in batchqueuepanel anyway
         Gtk::Label* lbq = Gtk::manage ( new Gtk::Label (M ("MAIN_FRAME_QUEUE")) );
 
-        if (options.mainNBVertical) {
-            lbq->set_angle (90);
-        }
-
+        lbq->set_angle (90);
         mainNB->append_page (*bpanel, *lbq);
 
 
@@ -581,22 +572,13 @@ RTWindow::RTWindow():
 
         setExpandAlignProperties (actionGrid, false, false, Gtk::ALIGN_CENTER, Gtk::ALIGN_CENTER);
 
-        if (options.mainNBVertical) {
-            prProgBar.set_orientation (Gtk::ORIENTATION_VERTICAL);
-            prProgBar.set_inverted (true);
-            actionGrid->set_orientation (Gtk::ORIENTATION_VERTICAL);
-            actionGrid->attach_next_to (prProgBar, Gtk::POS_BOTTOM, 1, 1);
-            actionGrid->attach_next_to (*preferences, Gtk::POS_BOTTOM, 1, 1);
-            actionGrid->attach_next_to (*btn_fullscreen, Gtk::POS_BOTTOM, 1, 1);
-            mainNB->set_action_widget (actionGrid, Gtk::PACK_END);
-        } else {
-            prProgBar.set_orientation (Gtk::ORIENTATION_HORIZONTAL);
-            actionGrid->set_orientation (Gtk::ORIENTATION_HORIZONTAL);
-            actionGrid->attach_next_to (prProgBar, Gtk::POS_RIGHT, 1, 1);
-            actionGrid->attach_next_to (*preferences, Gtk::POS_RIGHT, 1, 1);
-            actionGrid->attach_next_to (*btn_fullscreen, Gtk::POS_RIGHT, 1, 1);
-            mainNB->set_action_widget (actionGrid, Gtk::PACK_END);
-        }
+        prProgBar.set_orientation (Gtk::ORIENTATION_VERTICAL);
+        prProgBar.set_inverted (true);
+        actionGrid->set_orientation (Gtk::ORIENTATION_VERTICAL);
+        actionGrid->attach_next_to (prProgBar, Gtk::POS_BOTTOM, 1, 1);
+        actionGrid->attach_next_to (*preferences, Gtk::POS_BOTTOM, 1, 1);
+        actionGrid->attach_next_to (*btn_fullscreen, Gtk::POS_BOTTOM, 1, 1);
+        mainNB->set_action_widget (actionGrid, Gtk::PACK_END);
 
         actionGrid->show_all();
 
@@ -1213,9 +1195,6 @@ void RTWindow::setProgress(double p)
 
 void RTWindow::setProgressStr(const Glib::ustring& str)
 {
-    if (!options.mainNBVertical) {
-        prProgBar.set_text(str);
-    }
 }
 
 void RTWindow::setProgressState(bool inProcessing)
@@ -1431,11 +1410,8 @@ void RTWindow::createSetmEditor()
     setExpandAlignProperties (editorLabelGrid, false, false, Gtk::ALIGN_CENTER, Gtk::ALIGN_CENTER);
     Gtk::Label* const el = Gtk::manage (new Gtk::Label ( Glib::ustring (" ") + M ("MAIN_FRAME_EDITOR") ));
 
-    const auto pos = options.mainNBVertical ? Gtk::POS_TOP : Gtk::POS_RIGHT;
-
-    if (options.mainNBVertical) {
-        el->set_angle (90);
-    }
+    const auto pos = Gtk::POS_TOP;
+    el->set_angle(90);
 
     editorLabelGrid->attach_next_to (*Gtk::manage (new RTImage ("aperture.png")), pos, 1, 1);
     editorLabelGrid->attach_next_to (*el, pos, 1, 1);

rtdata/languages/default
@@ -851,7 +851,7 @@ MAIN_TOOLTIP_PREVIEWSHARPMASK;Preview the <b>sharpening contrast mask</b>.\nShor
 MAIN_TOOLTIP_QINFO;Quick info on the image.\nShortcut: <b>Shift-i</b>
 MAIN_TOOLTIP_SHOWHIDELP1;Show/Hide the left panel.\nShortcut: <b>l</b>
 MAIN_TOOLTIP_SHOWHIDERP1;Show/Hide the right panel.\nShortcut: <b>Ctrl-l</b>
-MAIN_TOOLTIP_SHOWHIDETP1;Show/Hide the top panel.\nShortcut: <b>Shift-l</b>
+MAIN_TOOLTIP_SHOWHIDETP1;Show/Hide the filmstrip panel.\nShortcut: <b>Shift-l</b>
 MAIN_TOOLTIP_THRESHOLD;Threshold
 MAIN_TOOLTIP_TOGGLE;Toggle the <b>Before</b>/<b>After</b> view.\nShortcut: <b>Shift-a</b>
 METADATA_LOAD_ERROR;Error loading metadata from %1: %2
@@ -1119,7 +1119,7 @@ PREFERENCES_SHOWEXPOSURECOMPENSATION;Append exposure compensation
 PREFERENCES_SHOWFILMSTRIPTOOLBAR;Show Filmstrip toolbar
 PREFERENCES_SHTHRESHOLD;Threshold for clipped shadows
 PREFERENCES_SINGLETAB;Single Editor Tab Mode
-PREFERENCES_SINGLETABVERTAB;Single Editor Tab Mode, Vertical Tabs
+PREFERENCES_SINGLETABBOTTOM;Single Editor Tab Mode, Filmstrip below Editor
 PREFERENCES_SND_HELP;Enter a full file path to set a sound, or leave blank for no sound.\nFor system sounds on Windows use "SystemDefault", "SystemAsterisk" etc., and on Linux use "complete", "window-attention" etc.
 PREFERENCES_SND_LNGEDITPROCDONE;Editor processing done
 PREFERENCES_SND_BATCHQUEUEDONE;Queue processing done

rtgui/editorpanel.cc
@@ -643,6 +643,9 @@ EditorPanel::EditorPanel (FilePanel* filePanel)
         tbTopPanel_1 = new Gtk::ToggleButton ();
         iTopPanel_1_Show = new RTImage ("panel-to-bottom.png");
         iTopPanel_1_Hide = new RTImage ("panel-to-top.png");
+        if (options.filmstripBottom) {
+            std::swap(iTopPanel_1_Show, iTopPanel_1_Hide);
+        }
         tbTopPanel_1->set_relief (Gtk::RELIEF_NONE);
         tbTopPanel_1->set_active (true);
         tbTopPanel_1->set_tooltip_markup (M ("MAIN_TOOLTIP_SHOWHIDETP1"));
@@ -848,8 +851,10 @@ EditorPanel::EditorPanel (FilePanel* filePanel)
     stb2->set_name("EditorToolbarBottom");
     stb2->add(*iops);
 
-    editbox->pack_start (*stb2, Gtk::PACK_SHRINK, 0);
     editbox->show_all ();
+
+    Gtk::VBox *vb = Gtk::manage(new Gtk::VBox());
+    stb2->show_all();
 
     // build screen
     hpanedl = Gtk::manage (new Gtk::Paned (Gtk::ORIENTATION_HORIZONTAL));
@@ -869,12 +874,22 @@ EditorPanel::EditorPanel (FilePanel* filePanel)
 
     if (filePanel) {
         catalogPane = new Gtk::Paned();
-        viewpaned->pack1 (*catalogPane, false, true);
+        if (options.filmstripBottom) {
+            viewpaned->pack2(*catalogPane, false, true);
+        } else {
+            viewpaned->pack1(*catalogPane, false, true);
+        }
     }
 
-    viewpaned->pack2 (*editbox, true, true);
+    if (options.filmstripBottom) {
+        viewpaned->pack1(*editbox, true, true);
+    } else {
+        viewpaned->pack2(*editbox, true, true);
+    }
 
-    hpanedl->pack2 (*viewpaned, true, true);
+    vb->pack_start(*viewpaned, true, true);
+    vb->pack_start(*stb2, Gtk::PACK_SHRINK, 0);
+    hpanedl->pack2(*vb, true, true);
 
     hpanedr->pack1 (*hpanedl, true, false);
     hpanedr->pack2 (*vboxright, false, false);

rtgui/options.cc
@@ -434,8 +434,8 @@ void Options::setDefaults()
     showFileNames = true;
     filmStripShowFileNames = false;
     tabbedUI = false;
-    mainNBVertical = true;
     multiDisplayMode = 0;
+    filmstripBottom = false;
     histogramPosition = 1;
     histogramRed = true;
     histogramGreen = true;
@@ -1378,8 +1378,8 @@ void Options::readFromFile(Glib::ustring fname)
                     showInfo = keyFile.get_boolean("GUI", "ShowInfo");
                 }
 
-                if (keyFile.has_key("GUI", "MainNBVertical")) {
-                    mainNBVertical = keyFile.get_boolean("GUI", "MainNBVertical");
+                if (keyFile.has_key("GUI", "FilmStripBottom")) {
+                    filmstripBottom = keyFile.get_boolean("GUI", "FilmStripBottom");
                 }
 
                 if (keyFile.has_key("GUI", "ShowClippedHighlights")) {
@@ -2039,7 +2039,7 @@ void Options::saveToFile(Glib::ustring fname)
         keyFile.set_boolean("GUI", "RememberZoomAndPan", rememberZoomAndPan);
         keyFile.set_boolean("GUI", "ShowHistory", showHistory);
         keyFile.set_boolean("GUI", "ShowInfo", showInfo);
-        keyFile.set_boolean("GUI", "MainNBVertical", mainNBVertical);
+        keyFile.set_boolean("GUI", "FilmStripBottom", filmstripBottom);
         keyFile.set_boolean("GUI", "ShowClippedHighlights", showClippedHighlights);
         keyFile.set_boolean("GUI", "ShowClippedShadows", showClippedShadows);
         keyFile.set_integer("GUI", "FrameColor", bgcolor);

rtgui/options.h
@@ -253,7 +253,7 @@ public:
     ThumbnailOrder thumbnailOrder;
     bool showHistory;
     bool showInfo;
-    bool mainNBVertical;  // main notebook vertical tabs?
+    bool filmstripBottom;
     bool showClippedHighlights;
     bool showClippedShadows;
     int highlightThreshold;

rtgui/preferences.cc
@@ -714,10 +714,10 @@ Gtk::Widget* Preferences::getGeneralPanel ()
     setExpandAlignProperties (flayoutlab, false, false, Gtk::ALIGN_START, Gtk::ALIGN_BASELINE);
     editorLayout = Gtk::manage (new MyComboBoxText ());
     setExpandAlignProperties (editorLayout, true, false, Gtk::ALIGN_FILL, Gtk::ALIGN_BASELINE);
-    editorLayout->append (M ("PREFERENCES_SINGLETAB"));
-    editorLayout->append (M ("PREFERENCES_SINGLETABVERTAB"));
-    editorLayout->append (M ("PREFERENCES_MULTITAB"));
-    editorLayout->append (M ("PREFERENCES_MULTITABDUALMON"));
+    editorLayout->append(M("PREFERENCES_SINGLETAB"));
+    editorLayout->append(M("PREFERENCES_SINGLETABBOTTOM"));
+    editorLayout->append(M("PREFERENCES_MULTITAB"));
+    editorLayout->append(M("PREFERENCES_MULTITABDUALMON"));
     editorLayout->set_active (2);
     Gtk::CellRendererText* cellRenderer = dynamic_cast<Gtk::CellRendererText*> (editorLayout->get_first_cell());
     cellRenderer->property_ellipsize() = Pango::ELLIPSIZE_MIDDLE;
@@ -1819,7 +1819,7 @@ void Preferences::storePreferences ()
     int editorMode = editorLayout->get_active_row_number();
     moptions.tabbedUI = (editorMode > 1);
     moptions.multiDisplayMode = editorMode == 3 ? 1 : 0;
-    moptions.mainNBVertical = editorMode == 1;
+    moptions.filmstripBottom = editorMode == 1;
 
     moptions.curvebboxpos = curveBBoxPosC->get_active_row_number();
     moptions.histogramPosition = ckbHistogramPositionLeft->get_active() ? 1 : 2;
@@ -2079,9 +2079,9 @@ void Preferences::fillPreferences ()
     useBundledProfiles->set_active(moptions.useBundledProfiles);
 
     if (!moptions.tabbedUI) {
-        editorLayout->set_active (moptions.mainNBVertical ? 1 : 0);
+        editorLayout->set_active(moptions.filmstripBottom ? 1 : 0);
     } else {
-        editorLayout->set_active (moptions.multiDisplayMode ? 3 : 2);
+        editorLayout->set_active(moptions.multiDisplayMode ? 3 : 2);
     }
 
     curveBBoxPosC->set_active (moptions.curvebboxpos);

commit 137ff824ccae59075ed35adf815ed69b942b3612

fixed layout in multi-editor mode, accidentally broken by 7af982c513a6cda947a769c5b82f17c1d6db7da0

rtgui/batchqueuepanel.cc
@@ -235,7 +235,7 @@ void BatchQueuePanel::updateTab (int qsize, int forceOrientation)
     }
 
     Gtk::Grid* grid = Gtk::manage (new Gtk::Grid ());
-    if ((forceOrientation == 0) || (forceOrientation == 2)) {
+    if ((forceOrientation == 0 && !options.tabbedUI) || (forceOrientation == 2)) {
         Gtk::Label* l;
 
         if(!qsize ) {

rtgui/filecatalog.cc
@@ -920,7 +920,7 @@ void FileCatalog::_refreshProgressBar ()
         Gtk::Label *label = nullptr;
 
         int tot = previewsToLoad ? previewsToLoad : previewsLoaded;
-        grid->attach_next_to(*Gtk::manage(new RTImage("folder-closed.png")), Gtk::POS_TOP, 1, 1);
+        grid->attach_next_to(*Gtk::manage(new RTImage("folder-closed.png")), options.tabbedUI ? Gtk::POS_RIGHT : Gtk::POS_TOP, 1, 1);
         int filteredCount = fileBrowser->getNumFiltered() < 0 ? tot : min(fileBrowser->getNumFiltered(), tot);
 
         label = Gtk::manage(new Gtk::Label(M("MAIN_FRAME_FILEBROWSER") +
@@ -928,9 +928,10 @@ void FileCatalog::_refreshProgressBar ()
                                                + Glib::ustring::format(tot) +
                                                (filteredCount != tot ? "]" : ")")));
 
-        label->set_angle(90);
-
-        grid->attach_next_to(*label, Gtk::POS_TOP, 1, 1);
+        if (!options.tabbedUI) {
+            label->set_angle(90);
+        }
+        grid->attach_next_to(*label, !options.tabbedUI ? Gtk::POS_TOP : Gtk::POS_RIGHT, 1, 1);
         grid->set_tooltip_markup(M("MAIN_FRAME_FILEBROWSER_TOOLTIP"));
         grid->show_all();
 

rtgui/rtwindow.cc
@@ -519,11 +519,17 @@ RTWindow::RTWindow():
         setExpandAlignProperties (fpanelLabelGrid, false, false, Gtk::ALIGN_CENTER, Gtk::ALIGN_CENTER);
         Gtk::Label* fpl = Gtk::manage (new Gtk::Label ( Glib::ustring (" ") + M ("MAIN_FRAME_EDITOR") ));
 
-        mainNB->set_tab_pos (Gtk::POS_LEFT);
-        fpl->set_angle (90);
-        RTImage* folderIcon = Gtk::manage (new RTImage ("folder-closed.png"));
-        fpanelLabelGrid->attach_next_to (*folderIcon, Gtk::POS_TOP, 1, 1);
-        fpanelLabelGrid->attach_next_to (*fpl, Gtk::POS_TOP, 1, 1);
+        if (!options.tabbedUI) {
+            mainNB->set_tab_pos (Gtk::POS_LEFT);
+            fpl->set_angle (90);
+            RTImage* folderIcon = Gtk::manage (new RTImage ("folder-closed.png"));
+            fpanelLabelGrid->attach_next_to (*folderIcon, Gtk::POS_TOP, 1, 1);
+            fpanelLabelGrid->attach_next_to (*fpl, Gtk::POS_TOP, 1, 1);
+        } else {
+            RTImage* folderIcon = Gtk::manage (new RTImage ("folder-closed.png"));
+            fpanelLabelGrid->attach_next_to (*folderIcon, Gtk::POS_RIGHT, 1, 1);
+            fpanelLabelGrid->attach_next_to (*fpl, Gtk::POS_RIGHT, 1, 1);
+        }
 
         fpanelLabelGrid->set_tooltip_markup (M ("MAIN_FRAME_FILEBROWSER_TOOLTIP"));
         fpanelLabelGrid->show_all ();
@@ -536,7 +542,9 @@ RTWindow::RTWindow():
         // decorate tab, the label is unimportant since its updated in batchqueuepanel anyway
         Gtk::Label* lbq = Gtk::manage ( new Gtk::Label (M ("MAIN_FRAME_QUEUE")) );
 
-        lbq->set_angle (90);
+        if (!options.tabbedUI) {
+            lbq->set_angle(90);
+        }
         mainNB->append_page (*bpanel, *lbq);
 
 
@@ -572,13 +580,22 @@ RTWindow::RTWindow():
 
         setExpandAlignProperties (actionGrid, false, false, Gtk::ALIGN_CENTER, Gtk::ALIGN_CENTER);
 
-        prProgBar.set_orientation (Gtk::ORIENTATION_VERTICAL);
-        prProgBar.set_inverted (true);
-        actionGrid->set_orientation (Gtk::ORIENTATION_VERTICAL);
-        actionGrid->attach_next_to (prProgBar, Gtk::POS_BOTTOM, 1, 1);
-        actionGrid->attach_next_to (*preferences, Gtk::POS_BOTTOM, 1, 1);
-        actionGrid->attach_next_to (*btn_fullscreen, Gtk::POS_BOTTOM, 1, 1);
-        mainNB->set_action_widget (actionGrid, Gtk::PACK_END);
+        if (!options.tabbedUI) {
+            prProgBar.set_orientation (Gtk::ORIENTATION_VERTICAL);
+            prProgBar.set_inverted (true);
+            actionGrid->set_orientation (Gtk::ORIENTATION_VERTICAL);
+            actionGrid->attach_next_to (prProgBar, Gtk::POS_BOTTOM, 1, 1);
+            actionGrid->attach_next_to (*preferences, Gtk::POS_BOTTOM, 1, 1);
+            actionGrid->attach_next_to (*btn_fullscreen, Gtk::POS_BOTTOM, 1, 1);
+            mainNB->set_action_widget (actionGrid, Gtk::PACK_END);
+        } else {
+            prProgBar.set_orientation (Gtk::ORIENTATION_HORIZONTAL);
+            actionGrid->set_orientation (Gtk::ORIENTATION_HORIZONTAL);
+            actionGrid->attach_next_to (prProgBar, Gtk::POS_RIGHT, 1, 1);
+            actionGrid->attach_next_to (*preferences, Gtk::POS_RIGHT, 1, 1);
+            actionGrid->attach_next_to (*btn_fullscreen, Gtk::POS_RIGHT, 1, 1);
+            mainNB->set_action_widget (actionGrid, Gtk::PACK_END);
+        }
 
         actionGrid->show_all();
 
@@ -1195,6 +1212,9 @@ void RTWindow::setProgress(double p)
 
 void RTWindow::setProgressStr(const Glib::ustring& str)
 {
+    if (options.tabbedUI) {
+        prProgBar.set_text(str);
+    }
 }
 
 void RTWindow::setProgressState(bool inProcessing)
@@ -1410,8 +1430,10 @@ void RTWindow::createSetmEditor()
     setExpandAlignProperties (editorLabelGrid, false, false, Gtk::ALIGN_CENTER, Gtk::ALIGN_CENTER);
     Gtk::Label* const el = Gtk::manage (new Gtk::Label ( Glib::ustring (" ") + M ("MAIN_FRAME_EDITOR") ));
 
-    const auto pos = Gtk::POS_TOP;
-    el->set_angle(90);
+    const auto pos = !options.tabbedUI ? Gtk::POS_TOP : Gtk::POS_RIGHT;
+    if (!options.tabbedUI) {
+        el->set_angle(90);
+    }
 
     editorLabelGrid->attach_next_to (*Gtk::manage (new RTImage ("aperture.png")), pos, 1, 1);
     editorLabelGrid->attach_next_to (*el, pos, 1, 1);