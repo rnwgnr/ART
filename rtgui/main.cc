5b8a799cd5c013f1a2e391b20c17e35a28f05cbf
Alberto Griggio <agriggio@users.noreply.github.com>
2022-09-03 12:40:31 +0200

    RawTherapee -> ART

--- rtgui/main.cc
+++ rtgui/main.cc
@@ -1,25 +1,25 @@
 /*
- *  This file is part of RawTherapee.
+ *  This file is part of ART.
  *
  *  Copyright (c) 2004-2010 Gabor Horvath <hgabor@rawtherapee.com>
  *
- *  RawTherapee is free software: you can redistribute it and/or modify
+ *  ART is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  RawTherapee is distributed in the hope that it will be useful,
+ *  ART is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with RawTherapee.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with ART.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifdef __GNUC__
 #if defined(__FAST_MATH__)
-#error Using the -ffast-math CFLAG is known to lead to problems. Disable it to compile RawTherapee.
+#error Using the -ffast-math CFLAG is known to lead to problems. Disable it to compile ART.
 #endif
 #endif
 
