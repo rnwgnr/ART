9a24f2567a452f0ca788f51e88b92b479002b149
Alberto Griggio <agriggio@users.noreply.github.com>
2024-05-01 09:10:31 +0200

    rawimage: properly extract the raw optical black for bayer sensors when using libraw

--- rtengine/rawimage.cc
+++ rtengine/rawimage.cc
@@ -823,6 +823,13 @@ int RawImage::loadRaw (bool loadData, unsigned int imageNum, bool closeFile, Pro
                 profile_data = new char[profile_length];
                 memcpy(profile_data, libraw_->imgdata.color.profile, profile_length);
             }
+
+            if (isBayer() && RT_blacklevel_from_constant == ThreeValBool::T && max(black, cblack[0], cblack[1], cblack[2], cblack[3]) == 0) {
+                auto &black_stat = libraw_->imgdata.color.black_stat;
+                for (int i = 0; i < 4; ++i) {
+                    cblack[i] = black_stat[i] / (1+black_stat[i+4]);
+                }
+            }
 #endif // ART_USE_LIBRAW
         }
 
