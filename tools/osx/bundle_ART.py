adf9987016dbd27e3137ced62fe9a1708dbee3af
Alberto Griggio <agriggio@users.noreply.github.com>
2025-01-31 08:33:43 +0100

    try to set halt_on_error=0 for address sanitizer in macOS debug builds

--- tools/osx/bundle_ART.py
+++ tools/osx/bundle_ART.py
@@ -416,7 +416,7 @@ export GTK_OVERLAY_SCROLLING=0
 export ART_EXIFTOOL_BASE_DIR="$d/Resources/exiftool"
 """)
             if opts.debug:
-                out.write('export ASAN_OPTIONS=detect_container_overflow=0:new_delete_type_mismatch=0\n')
+                out.write('export ASAN_OPTIONS=detect_container_overflow=0:new_delete_type_mismatch=0:halt_on_error=0\n')
                 out.write('$d/MacOS/.ART.bin "$@" 2>&1 | /usr/bin/tee ${HOME}/ART.log\n')
             else:
                 out.write('$d/MacOS/.ART.bin" "$@"\n')
